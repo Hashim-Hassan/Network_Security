2a7867c24493ab5f82e9cb79e9475f22ca8b06c1
Hashim-Hassan <md.hashim127@gmail.com>
2024-08-17 12:45:39 +0530

    exception and logger and get data updated

--- get_data.py
+++ get_data.py
@@ -3,17 +3,22 @@ import sys
 import json
 
 from dotenv import load_dotenv
+load_dotenv()
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+print(MONGO_DB_URL)
 
 
 import certifi
+ca = certifi.where()
+
 import pandas as pd
 import numpy as np
 import pymongo
-
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logger.logger import logging
 
-class NetwrokDataExtract():
+
+class NetworkDataExtract():
     def __init__(self):
         try:
             pass
@@ -25,12 +30,12 @@ class NetwrokDataExtract():
             pass
         except Exception as e:
             raise NetworkSecurityException(e, sys)
-        
+    
     def pushing_data_to_mongodb(self):
         try:
             pass
         except Exception as e:
-            raise NetworkSecurityException(e, sys)
+            raise NetworkSecurityException(e,sys)
         
-if __name__ == "__main__":
-    pass
+    if __name__ == '__main__':
+        pass
