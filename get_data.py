56da145e6c02d6eef99874487bfc27e91d635bbd
Hashim-Hassan <md.hashim127@gmail.com>
2024-08-18 01:13:17 +0530

    get_data.py updated

--- get_data.py
+++ get_data.py
@@ -3,9 +3,12 @@ import sys
 import json
 
 from dotenv import load_dotenv
-
+load_dotenv()
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+# print(MONGO_DB_URL)
 
 import certifi
+ca = certifi.where()
 import pandas as pd
 import numpy as np
 import pymongo
@@ -20,17 +23,36 @@ class NetwrokDataExtract():
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
-    def csv_to_json_convertor(self):
+    def csv_to_json_convertor(self, file_path):
         try:
-            pass
+            data = pd.read_csv(file_path)
+            data.reset_index(drop=True, inplace=True)
+            records = list(json.loads(data.T.to_json()).values())
+            return records
+        
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
-    def pushing_data_to_mongodb(self):
+    def pushing_data_to_mongodb(self, records, database, collection):
         try:
-            pass
+            self.database = database
+            self.collection = collection
+            self.records = records
+
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.mongo_client[self.database]
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return len(self.records)
+
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
 if __name__ == "__main__":
-    pass
+    FILE_PATH = "./Network_Data/NetworkData.csv"
+    DATABASE = "MLOps_Project"
+    COLLECTION = "NetworkData"
+    networkobj = NetwrokDataExtract()
+    records = networkobj.csv_to_json_convertor(FILE_PATH)
+    num_of_records = networkobj.pushing_data_to_mongodb(records, DATABASE, COLLECTION)
+    print(num_of_records)
